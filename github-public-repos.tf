8137decbeb7ea36125cccc0517a07853d0cfd9eb
Corne van Dyk <corne.vandyk@jumo.world>
2019-09-18 15:12:45 +0200

    import repos into state

---
+++ github-public-repos.tf
@@ -0,0 +1,135 @@
+resource "github_repository" "aws-lambda-deployments" {
+  name        = "aws-lambda-deployments"
+  private     = false
+  description = "testing aws lambda codepipelines"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
+resource "github_repository" "aws-lambda-deployments-safemode" {
+  name        = "aws-lambda-deployments-safemode"
+  private     = false
+  description = "Lambda deployments in safe mode"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
+resource "github_repository" "aws-testing" {
+  name        = "aws-testing"
+  private     = false
+  description = "null"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
+resource "github_repository" "cfn-nested-stacks-pipeline" {
+  name        = "cfn-nested-stacks-pipeline"
+  private     = false
+  description = "Pipeline to automate deployment of nested CFN stacks"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
+resource "github_repository" "cfn-nested-stacks-simpleexample" {
+  name        = "cfn-nested-stacks-simpleexample"
+  private     = false
+  description = "Simple example to test nested CFN stacks"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
+resource "github_repository" "codepipeline-cfntemplate" {
+  name        = "codepipeline-cfntemplate"
+  private     = false
+  description = "CloudFormation template to build a CodePipeline"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
+resource "github_repository" "codepipeline-simpleexample" {
+  name        = "codepipeline-simpleexample"
+  private     = false
+  description = "Simple test for basic CFN template with CodePipeline"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
+resource "github_repository" "ecs-refarch-cloudformation" {
+  name        = "ecs-refarch-cloudformation"
+  private     = false
+  description = " A reference architecture for deploying containerized microservices with Amazon ECS and AWS CloudFormation (YAML)"
+  has_wiki    = "false"
+  has_downloads = "true"
+  has_issues  = "false"
+  archived    = "false"
+}
+resource "github_repository" "esp8266" {
+  name        = "esp8266"
+  private     = false
+  description = "personal esp8266 projects"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
+resource "github_repository" "github-iac" {
+  name        = "github-iac"
+  private     = false
+  description = "Terraform IaC for Github"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
+resource "github_repository" "lambda-versioning-the-hard-way" {
+  name        = "lambda-versioning-the-hard-way"
+  private     = false
+  description = "Pipelines and CloudFormation template for safe-mode Lambda deploys without using SAM"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
+resource "github_repository" "permissions-boundary" {
+  name        = "permissions-boundary"
+  private     = false
+  description = "null"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
+resource "github_repository" "pipeline-variable-replacer" {
+  name        = "pipeline-variable-replacer"
+  private     = false
+  description = "CodePipeline for a Lambda function to do Jinja-based variable replacement in CloudFormation YAML files"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
+resource "github_repository" "private-APIgw-with-custom-dns" {
+  name        = "private-APIgw-with-custom-dns"
+  private     = false
+  description = "null"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
+resource "github_repository" "variable-replacer" {
+  name        = "variable-replacer"
+  private     = false
+  description = "Lambda function to do Jinja-based variable replacement in CloudFormation YAML files"
+  has_wiki    = "true"
+  has_downloads = "true"
+  has_issues  = "true"
+  archived    = "false"
+}
