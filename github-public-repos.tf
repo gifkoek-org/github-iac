b74b25863abe307c45207bab3902a32898a38843
Corne van Dyk <corne.vandyk@jumo.world>
2019-10-10 16:57:52 +0200

    build modules for legacy and new repos, modularise all existing repos

--- github-public-repos.tf
+++ github-public-repos.tf
@@ -1,18 +1,10 @@
 # ------------------------------
-# modularised repos
+# "new" repos
+# repos that were natively created by Terraform
+# the module driving this has "auto_init = True"
 # ------------------------------
-module "github_repo_aacorne-foobar" {
-  source           = "./modules"
-  repo_description = "Testing repo creation with TF"
-  repo_name        = "aacorne-testing1"
-  review_count     = 1
-  writer_team      = github_team.foobar.id
-  writer_team_slug = github_team.foobar.slug
-  reader_team      = github_team.read_only.id
-}
-
-module "github_repo_permissions-boundary" {
-  source           = "./modules"
+module "github_repo_aacorne-foobar2" {
+  source           = "./modules/new-github-repo"
   repo_description = "Testing repo creation with TF"
   repo_name        = "aacorne-testing2"
   review_count     = 1
@@ -21,143 +13,148 @@ module "github_repo_permissions-boundary" {
   reader_team      = github_team.read_only.id
 }
 
-module "github_repo_aacorne3" {
-  source           = "./modules"
-  repo_description = "Testing repo creation with TF"
-  repo_name        = "aacorne-testing3"
+# ------------------------------
+# legacy repos
+# repos that existed before being terraformed, ie these were "terraform import"ed
+# the module driving this has "auto_init = False"
+# ------------------------------
+module "github_repo_aws-lambda-deployments" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = "testing aws lambda codepipelines"
+  repo_name        = "aws-lambda-deployments"
   review_count     = 1
   writer_team      = github_team.foobar.id
   writer_team_slug = github_team.foobar.slug
   reader_team      = github_team.read_only.id
 }
 
-# ------------------------------
-# non-module repos
-# ------------------------------
-resource "github_repository" "aws-lambda-deployments" {
-  name          = "aws-lambda-deployments"
-  private       = false
-  description   = "testing aws lambda codepipelines"
-  has_wiki      = "true"
-  has_downloads = "true"
-  has_issues    = "true"
-  archived      = "false"
-}
-resource "github_repository" "aws-lambda-deployments-safemode" {
-  name          = "aws-lambda-deployments-safemode"
-  private       = false
-  description   = "Lambda deployments in safe mode"
-  has_wiki      = "true"
-  has_downloads = "true"
-  has_issues    = "true"
-  archived      = "false"
-}
-resource "github_repository" "aws-testing" {
-  name          = "aws-testing"
-  private       = false
-  description   = "null"
-  has_wiki      = "true"
-  has_downloads = "true"
-  has_issues    = "true"
-  archived      = "false"
-}
-resource "github_repository" "cfn-nested-stacks-pipeline" {
-  name          = "cfn-nested-stacks-pipeline"
-  private       = false
-  description   = "Pipeline to automate deployment of nested CFN stacks"
-  has_wiki      = "true"
-  has_downloads = "true"
-  has_issues    = "true"
-  archived      = "false"
-}
-resource "github_repository" "cfn-nested-stacks-simpleexample" {
-  name          = "cfn-nested-stacks-simpleexample"
-  private       = false
-  description   = "Simple example to test nested CFN stacks"
-  has_wiki      = "true"
-  has_downloads = "true"
-  has_issues    = "true"
-  archived      = "false"
-}
-resource "github_repository" "codepipeline-cfntemplate" {
-  name          = "codepipeline-cfntemplate"
-  private       = false
-  description   = "CloudFormation template to build a CodePipeline"
-  has_wiki      = "true"
-  has_downloads = "true"
-  has_issues    = "true"
-  archived      = "false"
-}
-resource "github_repository" "codepipeline-simpleexample" {
-  name          = "codepipeline-simpleexample"
-  private       = false
-  description   = "Simple test for basic CFN template with CodePipeline"
-  has_wiki      = "true"
-  has_downloads = "true"
-  has_issues    = "true"
-  archived      = "false"
-}
-resource "github_repository" "ecs-refarch-cloudformation" {
-  name          = "ecs-refarch-cloudformation"
-  private       = false
-  description   = " A reference architecture for deploying containerized microservices with Amazon ECS and AWS CloudFormation (YAML)"
-  has_wiki      = "false"
-  has_downloads = "true"
-  has_issues    = "false"
-  archived      = "false"
-}
-resource "github_repository" "esp8266" {
-  name          = "esp8266"
-  private       = false
-  description   = "personal esp8266 projects"
-  has_wiki      = "true"
-  has_downloads = "true"
-  has_issues    = "true"
-  archived      = "false"
-}
-resource "github_repository" "github-iac" {
-  name          = "github-iac"
-  private       = false
-  description   = "Terraform IaC for Github"
-  has_wiki      = "true"
-  has_downloads = "true"
-  has_issues    = "true"
-  archived      = "false"
-}
-resource "github_repository" "lambda-versioning-the-hard-way" {
-  name          = "lambda-versioning-the-hard-way"
-  private       = false
-  description   = "Pipelines and CloudFormation template for safe-mode Lambda deploys without using SAM"
-  has_wiki      = "true"
-  has_downloads = "true"
-  has_issues    = "true"
-  archived      = "false"
+module "github_repo_aws-lambda-deployments-safemode" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = "Lambda deployments in safe mode"
+  repo_name        = "aws-lambda-deployments-safemode"
+  review_count     = 1
+  writer_team      = github_team.foobar.id
+  writer_team_slug = github_team.foobar.slug
+  reader_team      = github_team.read_only.id
 }
 
-resource "github_repository" "pipeline-variable-replacer" {
-  name          = "pipeline-variable-replacer"
-  private       = false
-  description   = "CodePipeline for a Lambda function to do Jinja-based variable replacement in CloudFormation YAML files"
-  has_wiki      = "true"
-  has_downloads = "true"
-  has_issues    = "true"
-  archived      = "false"
+module "github_repo_aws-testing" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = "aws-testing"
+  repo_name        = "aws-testing"
+  review_count     = 1
+  writer_team      = github_team.foobar.id
+  writer_team_slug = github_team.foobar.slug
+  reader_team      = github_team.read_only.id
 }
-resource "github_repository" "private-APIgw-with-custom-dns" {
-  name          = "private-APIgw-with-custom-dns"
-  private       = false
-  description   = "null"
-  has_wiki      = "true"
-  has_downloads = "true"
-  has_issues    = "true"
-  archived      = "false"
+
+module "github_repo_cfn-nested-stacks-pipeline" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = "Pipeline to automate deployment of nested CFN stacks"
+  repo_name        = "cfn-nested-stacks-pipeline"
+  review_count     = 1
+  writer_team      = github_team.foobar.id
+  writer_team_slug = github_team.foobar.slug
+  reader_team      = github_team.read_only.id
 }
-resource "github_repository" "variable-replacer" {
-  name          = "variable-replacer"
-  private       = false
-  description   = "Lambda function to do Jinja-based variable replacement in CloudFormation YAML files"
-  has_wiki      = "true"
-  has_downloads = "true"
-  has_issues    = "true"
-  archived      = "false"
+
+module "github_repo_cfn-nested-stacks-simpleexample" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = "Simple example to test nested CFN stacks"
+  repo_name        = "cfn-nested-stacks-simpleexample"
+  review_count     = 1
+  writer_team      = github_team.foobar.id
+  writer_team_slug = github_team.foobar.slug
+  reader_team      = github_team.read_only.id
 }
+
+module "github_repo_codepipeline-cfntemplate" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = "CloudFormation template to build a CodePipeline"
+  repo_name        = "codepipeline-cfntemplate"
+  review_count     = 1
+  writer_team      = github_team.foobar.id
+  writer_team_slug = github_team.foobar.slug
+  reader_team      = github_team.read_only.id
+}
+
+module "github_repo_codepipeline-simpleexample" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = "Simple test for basic CFN template with CodePipeline"
+  repo_name        = "codepipeline-simpleexample"
+  review_count     = 1
+  writer_team      = github_team.foobar.id
+  writer_team_slug = github_team.foobar.slug
+  reader_team      = github_team.read_only.id
+}
+
+module "github_repo_ecs-refarch-cloudformation" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = "A reference architecture for deploying containerized microservices with Amazon ECS and AWS CloudFormation (YAML)"
+  repo_name        = "ecs-refarch-cloudformation"
+  review_count     = 1
+  writer_team      = github_team.foobar.id
+  writer_team_slug = github_team.foobar.slug
+  reader_team      = github_team.read_only.id
+}
+
+module "github_repo_esp8266" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = "personal esp8266 projects"
+  repo_name        = "esp8266"
+  review_count     = 1
+  writer_team      = github_team.foobar.id
+  writer_team_slug = github_team.foobar.slug
+  reader_team      = github_team.read_only.id
+}
+
+module "github_repo_github-iac" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = "Terraform IaC for Github"
+  repo_name        = "github-iac"
+  review_count     = 1
+  writer_team      = github_team.foobar.id
+  writer_team_slug = github_team.foobar.slug
+  reader_team      = github_team.read_only.id
+}
+
+module "github_repo_lambda-versioning-the-hard-way" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = "Pipelines and CloudFormation template for safe-mode Lambda deploys without using SAM"
+  repo_name        = "lambda-versioning-the-hard-way"
+  review_count     = 1
+  writer_team      = github_team.foobar.id
+  writer_team_slug = github_team.foobar.slug
+  reader_team      = github_team.read_only.id
+}
+
+module "github_repo_pipeline-variable-replacer" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = "CodePipeline for a Lambda function to do Jinja-based variable replacement in CloudFormation YAML files"
+  repo_name        = "pipeline-variable-replacer"
+  review_count     = 1
+  writer_team      = github_team.foobar.id
+  writer_team_slug = github_team.foobar.slug
+  reader_team      = github_team.read_only.id
+}
+
+module "github_repo_private-APIgw-with-custom-dns" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = ""
+  repo_name        = "private-APIgw-with-custom-dns"
+  review_count     = 1
+  writer_team      = github_team.foobar.id
+  writer_team_slug = github_team.foobar.slug
+  reader_team      = github_team.read_only.id
+}
+
+module "github_repo_variable-replacer" {
+  source           = "./modules/legacy-github-repo"
+  repo_description = "Lambda function to do Jinja-based variable replacement in CloudFormation YAML files"
+  repo_name        = "variable-replacer"
+  review_count     = 1
+  writer_team      = github_team.foobar.id
+  writer_team_slug = github_team.foobar.slug
+  reader_team      = github_team.read_only.id
+}
+
